993f7142d964509e4ce697295abad5d65ce49e5d
Dragonbestgg1 <dragonbestgg1@gmail.com>
2024-02-29 20:16:14 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import '../src/styles/App.css';
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
